ef166651bca66ef22d2c675a9dbd0e269952afca
adrianonobrega <adrianonobrega26@gmail.com>
2022-11-01 19:22:38 -0300

    Create documentation routes User

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,14 @@ import "express-async-errors"
 import express from "express"
 import appErrorMiddleware from "./middlewares/appErrorMiddleware"
 import appRoutes from "./routes"
+import swaggerUi from "swagger-ui-express"
+import swaggerDocument from "../swagger.json"
 
 const app = express()
 
 app.use(express.json())
+
+app.use('/api-docs',swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 appRoutes(app)
 
@@ -14,4 +18,6 @@ app.use(appErrorMiddleware)
 
 app.listen(5000, () => {
     console.log("server running on port 5000")
+
 })
+
