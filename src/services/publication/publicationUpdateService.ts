fe7b3a052e985a3c9da8f25a4f6f8829f49e8512
Osmar Faria <osm.faria@gmail.com>
2022-12-12 17:35:20 -0800

    hotfix: update image

--- src/services/publication/publicationUpdateService.ts
+++ src/services/publication/publicationUpdateService.ts
@@ -20,7 +20,7 @@ const publicationUpdateService = async (
       vehicle_type: data.vehicle_type,
       is_active: data.is_active,
       Image: {
-        create: data.images,
+        update: data.images,
       },
     },
   })
